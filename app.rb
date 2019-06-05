c34d2038daf3374ec1e4e2d9d1d0690c5238d4ab
Aaron Lipman <alipman88@gmail.com>
2019-06-04 20:27:19 -0400

    Use REST API in favor of direct SQL connection.
    (The slight lag in our direct SQL connection could theoretically result in sending duplicate messages.)
    Also, re-name the sms_opt_in custom action fields to sms_opt_in_synced -
    this ensures an action will never be re-synced by mistake.

--- app.rb
+++ app.rb
@@ -42,64 +42,84 @@ class Actionkit
   base_uri ENV['AK_API_PATH']
   basic_auth ENV['AK_API_USERNAME'], ENV['AK_API_PASSWORD']
 
-  def self.set_userfield user_id, name, value
-    existing_field = Actionkit.get("/rest/v1/userfield/?user=#{ user_id }&name=#{ name }").parsed_response["objects"][0]
+  def self.query sql
+    Actionkit.post("/rest/v1/report/run/sql/", body: { query: "#{ sql } -- cache buster: #{ Time.now.to_f } #{ rand }" })
+  end
+
+  def self.update_actionfield id, **opts
+    Actionkit.put("/rest/v1/actionfield/#{ id }/", body: opts)
+  end
+
+  def self.set_userfield user_id, **opts
+    existing_field = Actionkit.get("/rest/v1/userfield/?user=#{ user_id }&name=#{ opts[:name] }").parsed_response["objects"][0]
 
     if existing_field
-      Actionkit.put(existing_field['resource_uri'], body: {name: name, value: value})
+      Actionkit.put(existing_field['resource_uri'], body: {name: opts[:name], value: opts[:value]})
     else
-      Actionkit.post("/rest/v1/userfield/", body: {user: "/rest/v1/user/#{ user_id }/", name: name, value: value})
+      Actionkit.post("/rest/v1/userfield/", body: {user: "/rest/v1/user/#{ user_id }/", name: opts[:name], value: opts[:value]})
     end
   end
 end
 
 # Global variables
-set :next_phone_sql, File.read('next_phone.sql')
+set :next_phone_sql, File.read('next_phone.sql').gsub(/\n/, ' ')
+set :queue_length_sql, File.read('queue_length.sql').gsub(/\n/, ' ')
 set :revere_metadata_ids, {}
 
 # Syncs a single phone number to Revere
 # Returns 0 if an unsynced phone number is available, 60 if not
 def sync_single_phone
-  client = Mysql2::Client.new({
-    username: ENV['AK_USERNAME'],
-    password: ENV["AK_PASSWORD"],
-    host: ENV["AK_HOST"],
-    database: ENV["AK_DB"]
-  })
-  query_result = client.query(settings.next_phone_sql)
-  client.close
+  response_1 = Actionkit.query(Sinatra::Application.settings.next_phone_sql)
 
-  if query_result.first.nil?
-    return 60
-  else
-    msisdn = query_result.first["msisdn"].to_s.gsub(/\D/, '')
-
-    data = {
-      "msisdns" => [msisdn],
-      "mobileFlow" => query_result.first["revere_mobile_flow_id"] || ENV['DEFAULT_REVERE_MOBILE_FLOW_ID']
+  if response_1.code == 200 && response_1.parsed_response.class == Array && response_1.parsed_response.length > 0
+    query_result = {
+      "msisdn"                => response_1.parsed_response[0][0],
+      "revere_mobile_flow_id" => response_1.parsed_response[0][1],
+      "DFA ActionKit ID"      => response_1.parsed_response[0][2],
+      "First"                 => response_1.parsed_response[0][3],
+      "Last"                  => response_1.parsed_response[0][4],
+      "email"                 => response_1.parsed_response[0][5],
+      "zipcode"               => response_1.parsed_response[0][6],
+      "actionfield_id"        => response_1.parsed_response[0][7]
     }
 
-    Revere.post("/messaging/sendContent", body: data.to_json)
+    response_2 = Actionkit.update_actionfield query_result["actionfield_id"], name: "sms_opt_in_synced"
 
-    ["DFA ActionKit ID", "First", "Last", "email", "zipcode"].each do |field_name|
-      settings.revere_metadata_ids[field_name] ||= (Revere.metadata_field_id(field_name) || Revere.create_metadata_field(field_name))
-      data = { "id" => settings.revere_metadata_ids[field_name], "value" => query_result.first[field_name] }
-      Revere.put("/subscriber/addMetadata/#{ msisdn }", body: data.to_json)
+    if response_2.code == 204
+      msisdn = query_result["msisdn"].to_s.gsub(/\D/, '')
+
+      data = {
+        "msisdns" => [msisdn],
+        "mobileFlow" => query_result["revere_mobile_flow_id"] || ENV['DEFAULT_REVERE_MOBILE_FLOW_ID']
+      }
+
+      Revere.post("/messaging/sendContent", body: data.to_json)
+
+      ["DFA ActionKit ID", "First", "Last", "email", "zipcode"].each do |field_name|
+        settings.revere_metadata_ids[field_name] ||= (Revere.metadata_field_id(field_name) || Revere.create_metadata_field(field_name))
+        data = { "id" => settings.revere_metadata_ids[field_name], "value" => query_result[field_name] }
+        Revere.put("/subscriber/addMetadata/#{ msisdn }", body: data.to_json)
+      end
+
+      actionkit_id = query_result['DFA ActionKit ID']
+      Actionkit.set_userfield actionkit_id, name: 'most_recent_revere_sync', value: Time.now.to_s[0...19]
+
+      puts "phone: #{ msisdn }, actionkit_id: #{ actionkit_id }"
     end
 
-    actionkit_id = query_result.first['DFA ActionKit ID']
-    timestamp = query_result.first['created_at'].to_s[0...19]
-    Actionkit.set_userfield actionkit_id, 'most_recent_revere_sync', timestamp
-
-    puts "phone: #{ msisdn }, actionkit_id: #{ actionkit_id }"
     return 0
+  else
+    return 60
   end
 end
 
 if ENV['RACK_ENV'] == 'production'
   Thread.new do
     while true
-      sleep sync_single_phone
+      begin
+        sleep sync_single_phone
+      rescue
+      end
     end
   end
 end
@@ -109,13 +129,6 @@ get "/" do
 end
 
 get "/queue" do
-  client = Mysql2::Client.new({
-    username: ENV['AK_USERNAME'],
-    password: ENV["AK_PASSWORD"],
-    host: ENV["AK_HOST"],
-    database: ENV["AK_DB"]
-  })
-  result = client.query(File.read('queue_length.sql')).first.to_s
-  client.close
-  return result
+  response = Actionkit.query(Sinatra::Application.settings.queue_length_sql)
+  return response.parsed_response[0][0]
 end
--- next_phone.sql
+++ next_phone.sql
@@ -6,6 +6,7 @@ SELECT
   u.last_name AS Last,
   u.email,
   u.zip AS zipcode,
+  f.id AS actionfield_id,
   a.created_at
 FROM core_user u
 JOIN core_action a ON u.id = a.user_id
@@ -13,9 +14,7 @@ JOIN core_page p ON p.id = a.page_id
 JOIN core_actionfield f ON a.id = f.parent_id AND f.name = 'sms_opt_in'
 JOIN core_actionfield g ON a.id = g.parent_id AND g.name IN ('provided_mobile_phone', 'provided_phone')
 LEFT JOIN core_pagefield h ON p.id = h.parent_id AND h.name = 'revere_mobile_flow_id'
-LEFT JOIN core_userfield i ON u.id = i.parent_id AND i.name = 'most_recent_revere_sync' AND a.created_at <= i.value
 WHERE
-  i.id IS NULL AND
-  g.value RLIKE REPEAT('[0-9]{1}.*', 10) # 10-digit number
+  g.value RLIKE REPEAT('[0-9].*', 10)
 ORDER BY a.created_at
 LIMIT 1

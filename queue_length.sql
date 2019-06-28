d13eb0276af943af7af42dc858bd606bb4c236fc
Aaron Lipman <alipman88@gmail.com>
2019-06-28 12:55:16 -0400

    only sync core_actionfield records with value matching on, true or yes

--- queue_length.sql
+++ queue_length.sql
@@ -3,7 +3,7 @@ SELECT
 FROM core_user u
 JOIN core_action a ON u.id = a.user_id
 JOIN core_page p ON p.id = a.page_id
-JOIN core_actionfield f ON a.id = f.parent_id AND f.name = 'sms_opt_in'
+JOIN core_actionfield f ON a.id = f.parent_id AND f.name = 'sms_opt_in' AND f.value IN ('on', 'true', 'yes')
 JOIN core_actionfield g ON a.id = g.parent_id AND g.name IN ('provided_mobile_phone', 'provided_phone')
 LEFT JOIN core_pagefield h ON p.id = h.parent_id AND h.name = 'revere_mobile_flow_id'
 WHERE
